69ac8ce9d19f65b6d635d36f59dc79b0ba79a17a
Pancakem <pancakesdeath@protonmail.com>
2019-10-21 02:41:05 +0300

    set initial window size

--- src/Main.elm
+++ src/Main.elm
@@ -3,19 +3,33 @@ module Main exposing (..)
 import Browser
 import Html exposing (Html)
 import Model exposing (Model, initialModel)
-import Subscription exposing (subscriptions)
+import Model.Ui exposing (setWindowSize)
+import Subscription exposing (subscriptions, decodeTuple)
 import Update exposing (update)
 import View exposing (view)
+import Json.Decode exposing (Value)
 
 
 
 --------------------------------------------------------------------------- MAIN
 
 
-main : Program () Model Subscription.Msg
+init : Value -> (Model, Cmd msg)
+init flag = 
+  let
+      size = 
+          decodeTuple flag 
+      
+      newModel = 
+        {initialModel | ui = setWindowSize size initialModel.ui}
+          
+  in
+  (newModel, Cmd.none)
+
+main : Program Value Model Subscription.Msg
 main =
     Browser.element
-        { init = \_ -> ( initialModel, Cmd.none )
+        { init = init
         , update = update
         , view = view
         , subscriptions = subscriptions
